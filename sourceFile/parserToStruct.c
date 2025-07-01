0afaebe0f7b3f3a1ab4100f52530527069485568
JonathanZ <zez451@usask.ca>
2025-07-01 01:31:07 -0600

    now testing for compiling workflow.

---
+++ sourceFile/parserToStruct.c
@@ -0,0 +1,10 @@
+// We assume the detection of the first data section in the input file is:
+// to find a line of "UID: 001\n"
+
+#include "parserToStruct.h" // This header contains dataStruct.h
+
+DS* parseInputFile(const char* filename) {
+    print("the DS instance shall be returned\n");
+    return NULL;
+
+}
---
+++ sourceFile/parserToStruct.h
@@ -0,0 +1,9 @@
+#ifndef PARSERTOSTRUCT_H
+#define PARSERTOSTRUCT_H
+
+#include "dataStruct.h"
+
+// take a file name as input string, and return a struct of type DS
+DS* parseInputFile(const char* filename);
+
+#endif // PARSERTOSTRUCT_H

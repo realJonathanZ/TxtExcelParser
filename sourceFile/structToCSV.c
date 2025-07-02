b6027483b41b335ea24ef32003fdcbafd8c3b53b
JonathanZ <zez451@usask.ca>
2025-07-01 20:06:55 -0600

    don't know if possible but unique key counts varies in loop, thus not sure if can test uniqueness in loop

--- sourceFile/structToCSV.c
+++ sourceFile/structToCSV.c
@@ -3,21 +3,16 @@ will try to implement the logic in this file:
 input will be any (expected) un-NULL DS instance(defined inside dataStruct.h)
 output will be a CSV file with the data from the DS instance.
 output csv file will have format similar to example.csv
-
-I must find a way to gather all unique keys, they will
-soon become the header of the CSV file, and eventually become the first row
-in the style sheet.
-
-To do so, will need functionality to check if key is unique.
-
-If want to generate csv from datasection, will need:
-get all unique keys to be filled in first row, then,
-for the remaining rows, will iterate all kv pairs in every ds for values,
-therefore still need getValue() which shall stay in dataStruct.h
 */ 
 
+#include "structToCSV.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-/*Is a key string already existing in an array?
+
+
+/*Is a key string already existing in an keys array?
 * @param keys: array of strings to check element uniqueness
 * @param arrayLength: number of strings in the array 'keys'
 * @param key: the key to be checked whether it appears somewhere in the array
@@ -32,4 +27,13 @@ int isObtained(char** keys, int arrayLength, const char* key) {
     return 0; // False, cannot find any existence
 }
 
-
+/* collect all unique keys from all data sections
+* @param head: pointer to the head of the linked list of data sections
+* @param keys: pointer to an array of strings to store unique keys
+* @return uniqueKeys: an array conatining all unique keys found in the data sections
+*/
+char** getAllUniqKeys(DS* head, int* found_uniqkeys_quantity) {
+    // will use nested loop: for each section, need traverse all kv pairs
+    // and whether to put key inside is determined by uniqueness (the function above)
+    
+}
